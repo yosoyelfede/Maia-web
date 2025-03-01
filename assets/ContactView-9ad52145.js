b0027d58943b380378c24ec287c05f3190edf861
Fede Antunovic <federxv@gmail.com>
2025-03-01 03:12:49 -0300

    Updates

--- assets/ContactView-9ad52145.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c,a as n}from"./index-cef148be.js";const s={},a={class:"contact-view"};function r(_,t){return o(),c("div",a,t[0]||(t[0]=[n("h1",null,"Contacto",-1)]))}const l=e(s,[["render",r]]);export{l as default};
