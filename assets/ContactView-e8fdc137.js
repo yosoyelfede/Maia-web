1bcb7fe2457a09732d10e6e568023c95f4b2f3f3
Juan Pablo Rojas <jprojasc2@gmail.com>
2025-03-10 19:18:57 -0300

    Updates

---
+++ assets/ContactView-e8fdc137.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c,a as n}from"./index-f5958e8f.js";const s={},a={class:"contact-view"};function r(_,t){return o(),c("div",a,t[0]||(t[0]=[n("h1",null,"Contacto",-1)]))}const l=e(s,[["render",r]]);export{l as default};
